50ef0d15bcd6bf2c58fad86d803031d0eb98c881
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-11-26 21:57:06 -0800

    added ArticleList to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+import React from 'react';
 import './App.css';
 import Header from './Header.js';
-import Article from './Article.js';
 import Footer from './Footer.js'
+import ArticleList from './ArticleList';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Article />
-      <Article />
-      <Article />
-      <Article />
-      <Article />
-      <Article />
+      <ArticleList />
+      <ArticleList />
+      <ArticleList />
+      <ArticleList />
+      <ArticleList />
+      <ArticleList />
       <Footer />
     </div>
   );
