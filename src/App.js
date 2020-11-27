024811b89939f2093d24813752648eb74735fe9d
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-11-26 20:46:08 -0800

    added an article component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
+import Article from './Article.js';
 import Footer from './Footer.js'
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <Footer />
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+      <Footer />
     </div>
   );
 }
---
+++ src/Article.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Article extends Component {
+    render() {
+        return (
+            <div className="article">
+                <img className="horn-creature" src="" alt="creature" />
+                <div>This is a horn-creature article!</div>
+            </div>
+        );
+    }
+}
